20b9bfe1fa259e01a96cf2b39c62500e78c20606
Armando <armando.ortiz.jr.19@outlook.com>
2020-01-20 00:19:24 -0600

    Added decrypted method, fixed bugs.

--- encrypt_decrypt_file.rb
+++ encrypt_decrypt_file.rb
@@ -7,10 +7,20 @@ def encrypt(file_name, encrypt_key)
         line.each_char do |char|
             ascii_char = char.ord
             encrypted_char = (ascii_char.to_i ** encrypt_key[0]) % encrypt_key[1]
-            encrypt_file.write(encrypted_char)
+            encrypt_file.write(encrypted_char.to_s+"\n")
         end
     end
 
     encrypt_file.close
+end
 
+def decrypt(file_name, decrypt_key)
+    decrypt_file = File.open("decrypted_#{file_name.split("encrypted_")[1]}", "w")
+
+    File.foreach(file_name) do |line|
+        ascii = (line.chomp.to_i ** decrypt_key[0]) % decrypt_key[1]
+        decrypt_file.write(ascii.chr)
+    end
+
+    decrypt_file.close
 end
--- test.rb
+++ test.rb
@@ -1,5 +1,12 @@
 require_relative "./encrypt_decrypt_file.rb"
 rsa = RSA_Keys.new
 encrypt_key = rsa.e
+decrypt_key = rsa.d
+
+p rsa.p
+p rsa.q
+p encrypt_key
+p decrypt_key
 
 encrypt("secret.txt", encrypt_key)
+decrypt("encrypted_secret.txt", decrypt_key)
